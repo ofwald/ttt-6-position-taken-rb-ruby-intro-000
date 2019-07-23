e715686eceadcfee7f4f854f4cc50f41e308be85
Otto Wald <ofwald@gmail.com>
2019-07-23 20:15:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " " or board[index] == ""
+  if board[index] == nil
+    return false
+  elsif board[index] == " " or board[index] == ""
     return false
   end
 end
